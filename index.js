3d21e7e7d5671d47332b0b04a937b4e12a4779b4
mushayed125 <kmr18adi@gmail.com>
2025-01-19 19:20:39 +0600

    User related api added

--- index.js
+++ index.js
@@ -1,14 +1,13 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
-const { MongoClient, ServerApiVersion } = require('mongodb');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
 app.use(cors());
 app.use(express.json());
-
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bnuku.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -18,16 +17,29 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
+
+    const usersCollection = client.db("BloodLinkDB").collection("users");
+
+
+    // user related api
+    app.post("/register", async (req, res) => {
+      const userData = req.body;
+      const result = await usersCollection.insertOne(userData);
+      res.send(result);
+    });
+
     await client.connect();
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -35,12 +47,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
-app.get('/', (req, res) => {
-    res.send('BloodLink server is running')
-})
+app.get("/", (req, res) => {
+  res.send("BloodLink server is running");
+});
 
 app.listen(port, () => {
-    console.log(`Coffee Server is running on port: ${port}`);
-})
+  console.log(`Coffee Server is running on port: ${port}`);
+});
